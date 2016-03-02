518a4b617860bcf69affe679f0c686fd47ed8a68
David <guoxiaomin@jd.com>
2016-03-02 16:39:01 +0800

    Initial Commit

---
+++ WeChatDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChatDemo
+//
+//  Created by David Guo on 16/3/2.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChatDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChatDemo
+//
+//  Created by David Guo on 16/3/2.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
